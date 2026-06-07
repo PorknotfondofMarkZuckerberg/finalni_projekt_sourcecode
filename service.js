23f758ee39384b423d55882657283738e75763b0
PorknotfondofMarkZuckerberg <minecramfttoun@gmail.com>
2026-06-07 23:49:21 +0200

    Add files via upload

--- service.js
+++ service.js
@@ -7,3 +7,28 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker registration failed:', err);
         });
 }
+
+const CACHE_NAME = "movie-watchlist-v1";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/offline.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match("/offline.html");
+        })
+    );
+});
